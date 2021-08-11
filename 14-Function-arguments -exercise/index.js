59bfd654b36ebe94c79bccd43bd32e59ae9075ca
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-08-11 11:39:26 +0200

    Add files via upload

---
+++ 14-Function-arguments -exercise/index.js
@@ -0,0 +1,20 @@
+const Vermachine = function MachineMixer(rood, groen, northWall) {
+    console.log('verpot open maken')
+    console.log('kleur toevoegen')
+    console.log('verf roeren')
+    console.log('kwast in verf pot')
+    console.log('muur verven')
+    console.log('kwast terug plaatsen in pot')
+    console.log('muur is geverft en is klaar' + groen)
+    console.log('muur is geverft en is klaar' + rood)
+    console.log('muur is geverft en is klaar' + northWall)
+
+
+}
+Vermachine('muur is groen', 'muur is nu rood', "The north wall has been painted orange")
+
+//oproepen van een functie zonder krijgt je alleen de ver volgorde
+// de oproep volgore is belanrijk
+// dan krijgt de arguments een ander input
+
+
